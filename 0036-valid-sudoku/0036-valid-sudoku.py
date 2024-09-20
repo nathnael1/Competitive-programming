daa99b22715187968e4fcb63d23c98a0b562586e
nathnael1 <81173345+nathnael1@users.noreply.github.com>
2024-09-20 07:34:39 +0300

    Time: 211 ms (5.19%), Space: 28.3 MB (12.95%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,45 @@
+import numpy as np
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Check rows
+        row = set()
+        for i in range(len(board)):
+            for j in board[i]:
+                if j != ".":
+                    if j in row:
+                        return False
+                    row.add(j)
+            row = set()
+        
+        # Check columns
+        board = np.array(board)
+        transposed = np.transpose(board)
+        row = set()
+        for i in range(len(transposed)):
+            for j in transposed[i]:
+                if j!=".":
+                    if j in row:
+                        return False
+                    row.add(j)
+            row = set()
+
+        # Check subgrids
+        for k in range(9):
+            if not self.valid_subgrid(board, k):
+                return False
+
+        return True
+
+    def valid_subgrid(self, board: List[List[str]], subgrid_index: int) -> bool:
+        subgrid = set()
+        row_start = (subgrid_index // 3) * 3
+        column_start = (subgrid_index % 3) * 3
+        for i in range(3):
+            for j in range(3):
+                value = board[row_start + i][column_start + j]
+                if value != '.':
+                    if value in subgrid:
+                        return False
+                    subgrid.add(value)
+        return True
+
