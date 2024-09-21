bf5732b02cf3d3fa7b08026388a1e061a8b02fe6
nathnael1 <81173345+nathnael1@users.noreply.github.com>
2024-09-21 06:26:25 +0300

    Time: 39 ms (23.21%), Space: 16.5 MB (60.08%) - LeetHub

---
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.py
@@ -0,0 +1,5 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        #only 2 and 3 are strictly palindormic so there are no numbers palindromic 
+        # given an integer n>=4
+        return False
