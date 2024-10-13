6e2e75e674efe04fd9b7cd398420afb8e1a5a12f
nathnael1 <81173345+nathnael1@users.noreply.github.com>
2024-10-13 07:19:36 +0300

    Time: 67 ms (5.78%), Space: 17 MB (7.51%) - LeetHub

---
+++ 1044-find-common-characters/1044-find-common-characters.py
@@ -0,0 +1,39 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        res = {}
+        result = []
+        container = {}
+        min_value = []
+        valid = set()
+        for word in words:
+            temp = {}
+            for letter in word:
+                res[letter] = res.get(letter,0)+1
+                temp[letter] = temp.get(letter, 0) + 1
+            for letter,count in temp.items():
+                min_value.append([letter,count])
+        for let, rep in min_value:
+            res[let] = min(res[let],rep)
+        for word in words:
+            temp = set()
+            for letter in word:
+                if letter in temp:
+                    continue
+                container[letter] = container.get(letter,0)+1
+                temp.add(letter)
+        for items,value in container.items():
+            if value == len(words):
+                valid.add(items)
+        for items,value in res.items():
+            if items in valid:
+                i  = value
+                while i > 0:
+                    result.append(items)
+                    i-=1
+    
+        print(res)
+        print(valid)
+        print(min_value)
+        return result
+
+        
