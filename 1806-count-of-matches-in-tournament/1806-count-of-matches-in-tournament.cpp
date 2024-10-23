9d05cabf9c3b7f8bf800c23afc30ee0fcbb9a25c
nathnael1 <81173345+nathnael1@users.noreply.github.com>
2024-10-23 13:35:41 +0300

    Time: 3 ms (43.05%), Space: 7.5 MB (51.69%) - LeetHub

--- 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.cpp
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        int matches = 0;
-        int teams = n;
-        while(teams > 1){
-            if(teams %2 == 0){
-                matches+= teams/2;
-                teams = teams/2;
-            }else{
-                matches+= teams/2;
-                teams = (teams/2) + 1;
-            }
-        }
-        return matches;
+        return n-1;
     }
 };
