5cbaf850aac6e085acf7347331008c84ff5d388c
nathnael1 <81173345+nathnael1@users.noreply.github.com>
2024-11-11 22:17:30 +0300

    Time: 11 ms (33.2%), Space: 18.4 MB (7.59%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,7 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hashed = defaultdict(int)
+        for item in nums:
+            hashed[item]+=1
+        value = max(hashed, key = hashed.get)
+        return value
