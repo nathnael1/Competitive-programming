84e8e1cca2789f53bcea78807e6993840f057b7a
nathnael1 <81173345+nathnael1@users.noreply.github.com>
2024-11-20 19:10:01 +0300

    Time: 0 ms (100%), Space: 16.6 MB (49.87%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        res = []
+        for i in s:
+            if i == "(":
+                res.append(")")
+            elif i == "[":
+                res.append("]")
+            elif i == "{":
+                res.append("}")
+            elif not res or res.pop()!=i:
+                return False
+        return not res
