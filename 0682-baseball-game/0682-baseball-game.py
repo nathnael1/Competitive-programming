7b81415e0d312aff2527edd8b2b32bd4f9f04a89
nathnael1 <81173345+nathnael1@users.noreply.github.com>
2024-12-17 06:00:57 -0800

    Time: 1 ms (35.29%), Space: 17.8 MB (6.2%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -1,13 +1,14 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
         res = []
-        for item in operations:
-            if item == "D":
-                res.append(res[-1] * 2)
-            elif item == "C":
-                res.pop()
-            elif item == "+":
+        
+        for i in operations:
+            if i == '+':
                 res.append(res[-1] + res[-2])
+            elif i == 'D':
+                res.append(res[-1] * 2)
+            elif i == 'C':
+                res.pop()
             else:
-                res.append(int(item))
+                res.append(int(i))
         return sum(res)
