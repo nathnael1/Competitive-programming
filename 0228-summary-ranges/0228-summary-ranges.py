7f13314acc4232fda6b940a523f922a13b4bfad0
nathnael1 <81173345+nathnael1@users.noreply.github.com>
2024-12-16 22:15:22 -0800

    Time: 0 ms (100%), Space: 17.3 MB (16.42%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -5,17 +5,14 @@ class Solution:
         res = []
         start = nums[0]
         for i in range(1,len(nums)):
-
-            if nums[i] != nums[i-1] + 1:
-                if start == nums[i-1]:
-                    res.append(f"{start}")
-                else:
+            if nums[i] - nums[i-1] !=1:
+                if nums[i-1]!=start:
                     res.append(f"{start}->{nums[i-1]}")
+                else:
+                    res.append(f"{start}")
                 start = nums[i]
-        if start == nums[-1]:
-            res.append(f"{start}")
-        else:
+        if start!=nums[-1]:
             res.append(f"{start}->{nums[-1]}")
-                
-            
+        else:
+            res.append(f"{nums[-1]}")
         return res
