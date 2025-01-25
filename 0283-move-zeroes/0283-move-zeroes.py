7c1cefc5ffc27411e935edfe49fb3ce6017e8918
Nathnael Tamirat <81173345+nathnael-tamriat@users.noreply.github.com>
2025-01-25 10:59:07 -0800

    Time: 3 ms (82.24%), Space: 18.8 MB (52.06%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,10 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        write = 0
-        read = 0
-        while read < len(nums):
-            if nums[read] != 0:
-                nums[read], nums[write] = nums[write], nums[read]
-                write+=1
-            read+=1
+        non_zero = 0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[i],nums[non_zero] = nums[non_zero],nums[i]
+                non_zero+=1
+        
