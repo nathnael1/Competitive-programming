4ca4c5cd837384ef96a68b920416226743258910
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-01-28 03:49:25 -0800

    Time: 11 ms (83.5%), Space: 20.8 MB (52.55%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashed = {}
-        for i in strs:
-            sorted_word = ''.join(sorted(i))
-            if sorted_word not in hashed:
-                hashed[sorted_word] = []
-            hashed[sorted_word].append(i)
-        return [value for item,value in hashed.items()]
-       
+        res = defaultdict(list)
+        for items in strs:
+            item = ''.join(sorted(items))
+            res[item].append(items)
+        return [val for item,val in res.items()]
