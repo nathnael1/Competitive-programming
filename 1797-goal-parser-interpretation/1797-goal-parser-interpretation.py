fde8d45e1e62a691b7072af1ec0bdb38dba498e9
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-01-29 23:20:20 -0800

    Time: 44 ms (14.39%), Space: 17.9 MB (17.84%) - LeetHub

---
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
@@ -0,0 +1,15 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        res = ""
+        i = 0
+        while i < len(command):
+            if command[i] == "G":
+                res+="G"
+                i+=1
+            elif command[i:i+2] == "()":
+                res+="o"
+                i+=2
+            elif command[i:i+4] == "(al)":
+                res += "al"
+                i+=4
+        return res
