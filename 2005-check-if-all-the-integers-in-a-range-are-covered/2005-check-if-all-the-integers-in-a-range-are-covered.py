d228a995f7bada2fe31f5cc8ec04dd09bd66726c
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-03 04:35:39 -0800

    Time: 2 ms (20.58%), Space: 18 MB (12.22%) - LeetHub

--- 2005-check-if-all-the-integers-in-a-range-are-covered/2005-check-if-all-the-integers-in-a-range-are-covered.py
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/2005-check-if-all-the-integers-in-a-range-are-covered.py
@@ -1,12 +1,15 @@
 class Solution:
     def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
         #saving all left and right range in original set
-        my_set=set()
-        for start,end in ranges:
-            for i in range(start,end+1):
-                my_set.add(i)
-        for j in range(left,right+1):
-            if j not in my_set:
-                return False
-        return True
+        original_set = {num for num in range(left,right+1)}
         #going through each loop
+        for i in range(len(ranges)):
+            left_range = ranges[i][0]
+            right_range = ranges[i][1]
+            #and checking each element exist in a set and if it exist removing it from the original set
+            for j in range(left_range,right_range+1):
+                if j in original_set:
+                    original_set.discard(j)
+            if not original_set:
+                return True
+        return not original_set
