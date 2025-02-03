747a52cbc5b6bfbb0e57dcbf598ca42cfb83369e
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-03 03:20:23 -0800

    Time: 0 ms (100%), Space: 19.4 MB (5.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashed = {}
+        #create a dictionary
+        two_sum_dict = {}
+        #itterate through nums
         for i in range(len(nums)):
-            diff = target - nums[i]
-            if nums[i] not in hashed:
-                hashed[diff] = i
-                continue
-            return [i,hashed[nums[i]]]
-        
+            #if  target-nums not in the dict add in the dict as key and value as index
+            diff = target-nums[i]
+            #if target-nums exist in dict it means we find the value so return the itterator
+            #i and the value of the dict
+            if nums[i] not in two_sum_dict:
+                two_sum_dict[diff] = i
+            else:
+                return [i,two_sum_dict[nums[i]]]
