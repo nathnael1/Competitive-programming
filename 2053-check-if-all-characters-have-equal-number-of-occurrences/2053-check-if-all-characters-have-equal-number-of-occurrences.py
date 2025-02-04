22c45aba84f9d28375b4b55ba0905c4ffffd7042
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-04 08:05:56 -0800

    Time: 0 ms (100%), Space: 17.9 MB (41.13%) - LeetHub

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.py
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -1,8 +1,10 @@
 class Solution:
     def areOccurrencesEqual(self, s: str) -> bool:
-        count = Counter(s)
-        checker = count[s[0]]
-        for key,value in count.items():
-            if value!=checker:
+        #create a counter for s
+        counter_s = Counter(s)
+        first_value = counter_s[s[0]]
+        for value in counter_s.values():
+        #check if there is a defection from the value of the first counter
+            if value != first_value:
                 return False
         return True
