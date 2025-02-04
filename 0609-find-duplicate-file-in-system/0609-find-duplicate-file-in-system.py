69028c3b467996f7138913f9f1b4d762967b24c4
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-03 22:54:15 -0800

    Time: 24 ms (27.56%), Space: 25.1 MB (57.89%) - LeetHub

--- 0609-find-duplicate-file-in-system/0609-find-duplicate-file-in-system.py
+++ 0609-find-duplicate-file-in-system/0609-find-duplicate-file-in-system.py
@@ -1,21 +1,21 @@
 from collections import defaultdict
 class Solution:
     def findDuplicate(self, paths: List[str]) -> List[List[str]]:
-        hashed = defaultdict(list)
-        for path in paths:
-            dirr, *files = path.split()
-            for f in files:
-                fname, fcont = f.split('(')
-                fcont = fcont[:-1]
-                hashed[fcont].append(f"{dirr}/{fname}")
-        ans = []
+        my_dict = defaultdict(list)
+        #itterating through the list
+        for item in paths:
+            directory,*file_name = item.split()
+            for unique_file in file_name:
+                start = unique_file.find("(") +1
+                end = unique_file.find(")") 
+                content = unique_file[start:end]
+                file_type = unique_file[:start-1]
+                name = directory+"/"+file_type
+                my_dict[content].append(name)
+        res = []
+        for item,value in my_dict.items():
+            if len(value) > 1:
+                res.append(value)
+        return res
 
-        for item,value in hashed.items():
-           if len(value) > 1:
-                ans.append(value)
-        return ans
-
-
-
-        
                 
