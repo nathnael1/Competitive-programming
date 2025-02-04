06e8c479b2c51c307323a6f5eb052c1d5f25f067
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-04 08:41:42 -0800

    Time: 62 ms (86.41%), Space: 18.1 MB (42.56%) - LeetHub

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -4,11 +4,9 @@ class Solution:
         #create a counter for s and t
         counter_s = Counter(s)
         counter_t = Counter(t)
-        #increment by one for unique elements in t and take the difference if
-        #character t is greater than character s increment the deference
+
         for key,value in counter_t.items():
-            if key in counter_s and value > counter_s[key]:
+            if value > counter_s[key]:
                 operation += abs(value - counter_s[key])
-            elif key not in counter_s:
-                operation += 1
+
         return operation
