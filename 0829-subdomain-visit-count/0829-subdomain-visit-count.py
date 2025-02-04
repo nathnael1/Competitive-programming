7250163c30e5b2d8fd71567affc7af1269f563fd
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-04 03:23:50 -0800

    Time: 11 ms (6.62%), Space: 18.1 MB (9.82%) - LeetHub

--- 0829-subdomain-visit-count/0829-subdomain-visit-count.py
+++ 0829-subdomain-visit-count/0829-subdomain-visit-count.py
@@ -1,14 +1,28 @@
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
-        countall = defaultdict(int)
-        for temp in cpdomains:
-            count,domain = temp.split()
-            count= int(count)
-            subdomain = domain.split('.')
-            for i in range(len(subdomain)):
-                tempp = '.'.join(subdomain[i:])
-                countall[tempp]+=count
-        ans = []
-        for domain, count in countall.items():
-            ans.append(str(count) + " " + domain )
-        return ans
+        my_dict = defaultdict(int)
+        #splitting the number and the domains and splitting the domains by agian
+        for item in cpdomains:
+            number,domains = item.split()
+            number = int(number)
+            
+            #we are going reverse and find . and each time we are going to append it to our array
+            #so we have to create 2 arrays one for adding the word and one for appending it to the main
+            word = []
+            all_domains = []
+            for ch in domains[::-1]:
+                if ch == '.':
+                    all_domains.append("".join(word[::-1]))
+                    word.append(ch)
+                else:
+                    word.append(ch)
+            all_domains.append("".join(word[::-1]))
+            print(all_domains)
+            #for each domain_name we are going to use a key and incrementing it by value of number
+            for each_domain_name in all_domains:
+                my_dict[each_domain_name]+=number
+        #printing the output in our desired array res
+        res = []
+        for item,value in my_dict.items():
+            res.append(f"{value} {item}")
+        return res
