af0ac9166dc1493246b5f07d969d69decb19ea92
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-03 23:30:08 -0800

    Time: 0 ms (100%), Space: 17.8 MB (45.06%) - LeetHub

--- 0722-remove-comments/0722-remove-comments.py
+++ 0722-remove-comments/0722-remove-comments.py
@@ -1,26 +1,30 @@
 class Solution:
     def removeComments(self, source: List[str]) -> List[str]:
+        #creating res variable to store the result
         res = []
-        new_line = []
+        #creating a flag that store is_block or ntot default value of false
         is_block = False
+        new_line = []
+        #going through each line
         for line in source:
             i = 0
             while i < len(line):
+            #checking each character and determining based on conditions of /* */ and //
                 if not is_block:
                     if line[i:i+2] == "/*":
-                        is_block = True
                         i+=1
+                        is_block = True
                     elif line[i:i+2] == "//":
                         break
                     else:
                         new_line.append(line[i])
                 else:
                     if line[i:i+2] == "*/":
-                        is_block = False
                         i+=1
+                        is_block = False
                 i+=1
             if new_line and not is_block:
                 res.append("".join(new_line))
                 new_line = []
-
+                
         return res
