a8c2396e94b5a718c507932a92a089d359a22091
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-05 04:55:39 -0800

    Time: 0 ms (100%), Space: 17.7 MB (75.88%) - LeetHub

--- 1651-shuffle-string/1651-shuffle-string.py
+++ 1651-shuffle-string/1651-shuffle-string.py
@@ -1,8 +1,9 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
+        #we have to create res array with same length 
         res = [""] * len(s)
 
         for index,value in enumerate(indices):
+            #inserting the element to res with given indices
             res[value] = s[index]
         return "".join(res)
-            
