f35af7e30b29a300efebdcfd95cabd35d4a17ee7
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-05 03:47:32 -0800

    Time: 8 ms (92.99%), Space: 20.7 MB (61.12%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,7 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
-        for items in strs:
-            item = ''.join(sorted(items))
-            res[item].append(items)
-        return [val for item,val in res.items()]
+        #creating a default dict that stores sorted word as a key
+        anagram_storage = defaultdict(list)
+        #going through loop and using the sorted word as a key and appending the word as a value
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            anagram_storage[sorted_word].append(word)
+        #returning in the desired format
+        return [anagrams for key,anagrams in anagram_storage.items()]
