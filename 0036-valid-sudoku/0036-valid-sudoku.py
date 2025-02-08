2eb8baa42e08ddd13aca0dddc7a017eedd347730
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-08 05:24:22 +0300

    Time: 8 ms (18.84%), Space: 17.8 MB (39.56%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,44 +1,40 @@
-import numpy as np
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-      
-        row = set()
-        for i in range(len(board)):
-            for j in board[i]:
-                if j != ".":
-                    if j in row:
+
+        def horizontal_check(matrix):
+            #appending each row in a set
+            for i in range(len(matrix)):
+                seen = set()
+                for j in range(len(matrix[0])):
+                    if matrix[i][j] in seen:
                         return False
-                    row.add(j)
-            row = set()
+                    if matrix[i][j] != ".":
+                        seen.add(matrix[i][j])
+            return True
         
-        board = np.array(board)
-        transposed = np.transpose(board)
-        row = set()
-        for i in range(len(transposed)):
-            for j in transposed[i]:
-                if j!=".":
-                    if j in row:
-                        return False
-                    row.add(j)
-            row = set()
+        def vertical_check(matrix):
+            #creating transpose
+            row_length = len(matrix)
+            col_length = len(matrix[0])
+            copied_matrix = [[0]*row_length for _ in range(col_length)]
+            for i in range(row_length):
+                for j in range(col_length):
+                    copied_matrix[i][j] = matrix[j][i]
 
+            #calling the horizontal_check
+            return horizontal_check(copied_matrix)
         
-        for k in range(9):
-            if not self.valid_subgrid(board, k):
-                return False
-
-        return True
-
-    def valid_subgrid(self, board: List[List[str]], subgrid_index: int) -> bool:
-        subgrid = set()
-        row_start = (subgrid_index // 3) * 3
-        column_start = (subgrid_index % 3) * 3
-        for i in range(3):
-            for j in range(3):
-                value = board[row_start + i][column_start + j]
-                if value != '.':
-                    if value in subgrid:
-                        return False
-                    subgrid.add(value)
-        return True
-
+        def sub_grid_check(matrix):
+            
+            for row in range(0,9,3):
+                for col in range(0,9,3):
+                    seen = set()
+                    for i in range(3):
+                        for j in range(3):
+                            if matrix[row+i][col+j] in seen:
+                                return False
+                            if matrix[row+i][col+j] != ".":
+                                seen.add(matrix[row+i][col+j])
+            return True
+        return horizontal_check(board) and vertical_check(board) and sub_grid_check(board)
+                        
