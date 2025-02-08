9cac65219b408038edb2d8445d6028afa8cef22b
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-08 04:50:22 +0300

    Time: 11 ms (65.47%), Space: 20.9 MB (13.77%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        #output array
+        res = []
+
+        #using dictionary to append when diagonal sum is equal 
+        neg_diagonal = defaultdict(list)
+        pos_diagonal = defaultdict(list)
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                neg_diagonal[i+j].append(mat[i][j])
+
+        #returning in the desired format
+
+        top = True
+        for value in neg_diagonal.values():
+            if top == True:
+                value.reverse()
+                for item in value:
+                    res.append(item)
+                top = False
+            else:
+                for item in value:
+                    res.append(item)
+                top = True
+        return res
+
