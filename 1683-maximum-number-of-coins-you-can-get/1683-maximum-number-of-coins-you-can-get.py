0b5b1bb6bb5a92524f1d577570121ad996020a0f
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-11 11:32:45 +0300

    Time: 752 ms (7.77%), Space: 24.7 MB (94.82%) - LeetHub

--- 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.py
+++ 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.py
@@ -1,12 +1,10 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
-        piles.sort(reverse = True)
+        #poping the first, second and the third  taking the value of the second
+        piles.sort()
         res = 0
-        counter = 0
-        max_coin = len(piles)/3
-        for i in range(1,len(piles),2):
-            if counter == max_coin:
-                break
-            res += piles[i]
-            counter+=1
+        while piles:
+            piles.pop()
+            res += piles.pop()
+            piles.pop(0)
         return res
