a5491a7ffb78a10663c73441932ca2f2f0157678
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-11 11:19:18 +0300

    Time: 7 ms (7.11%), Space: 18.1 MB (32.87%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.py
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.py
@@ -1,7 +1,17 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        #returning ans[i] = nums[nums[i]]
-        ans = [0]  * len(nums)
+        #returning we have to use meta = a+ b*1001
+        #where a = nums[i] and b = nums[nums[i]]
+        
+        const_n = 1001
         for i in range(len(nums)):
-            ans[i] = nums[nums[i]]
-        return ans
+            a = nums[i]
+            b = nums[a]%const_n
+            nums[i] = a + b*const_n
+    
+
+        #decrypting meta
+        for i in range(len(nums)):
+            nums[i]//=const_n
+
+        return nums
