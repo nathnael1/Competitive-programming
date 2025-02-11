6064cf751b18cb7b3c5b615d0395dece86b3e611
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-11 10:24:43 +0300

    Time: 196 ms (34.53%), Space: 18.7 MB (71.49%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.py
+++ 0661-image-smoother/0661-image-smoother.py
@@ -1,70 +1,33 @@
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
-        #creating duplicated array
+        move = [(0,-1),(0,1),(-1,0),(1,0),(-1,-1),(1,1),(-1,1),(1,-1)]
+
         row_length = len(img)
         col_length = len(img[0])
-        res_matrix = [[0]*col_length for i in range(row_length)]
-        #iterrating through the array
-        for row_index in range(len(img)):
-            #total_sum and number of items
 
-            for col_index in range(len(img[row_index])):
-                total_sum=img[row_index][col_index]
-                total_count = 1
-                #same column and one row up
-                if row_index != 0:
-                    #incremening count and adding total_sum
-                    total_sum += img[row_index-1][col_index]
-                    total_count +=1
-                 #same column and one row down
-                if row_index != len(img) - 1:
-                    #incremening count and adding total_sum
-                    total_sum += img[row_index+1][col_index]
-                    total_count +=1
-                # one row up and one column left
-                if row_index != 0 and col_index!=0:
-                    #incremening count and adding total_sum
-                    total_sum += img[row_index-1][col_index-1]
-                    total_count +=1
+        res = [[0]*col_length for _ in range(row_length)]
 
-                # one row up and one column right
-                if row_index != 0 and col_index!=len(img[row_index]) - 1:
-                    #incremening count and adding total_sum
-                    total_sum += img[row_index-1][col_index+1]
-                    total_count +=1
+        def check(i,j):
+            return ( i < row_length and i >= 0) and ( j < col_length and j >= 0)
 
-                # one row down and one column left
-                if row_index != len(img) - 1 and col_index!=0:
-                    #incremening count and adding total_sum
-                    total_sum += img[row_index+1][col_index-1]
-                    total_count +=1
+        #finding all sum_values and number of valid elements
+        def evaluate(i,j):
+            total_sum = 0
+            total_length = 0
+            for row,col in move:
+                new_i = row+i
+                new_j = col + j
+                if check(new_i,new_j):
+                    total_sum += img[new_i][new_j]
+                    total_length += 1
+            total_sum += img[i][j]
+            total_length += 1
+            return total_sum,total_length
 
-                # same row right column
-
-                if col_index != (len(img[0]) - 1):
-                    
-                    #incremening count and adding total_sum
-                    total_sum += img[row_index][col_index + 1]
-                    total_count +=1
-
-                           
-                # one row down and one column right
-                if row_index != len(img) - 1 and col_index!=len(img[row_index]) - 1:
-                    #incremening count and adding total_sum
-                    total_sum += img[row_index+1][col_index+1]
-                    total_count +=1
-
-                #same row left column
-                if col_index != 0:
-                    #incremening count and adding total_sum
-                    total_sum += img[row_index][col_index-1]
-                    total_count +=1
-                
-                average_value = total_sum//total_count
-                res_matrix[row_index][col_index] = average_value
-        return res_matrix
-
-                
-
-                
-
+        #construct the matrix
+        for i in range(row_length):
+            for j in range(col_length):
+                total_sum,total_length = evaluate(i,j)
+                res[i][j] = total_sum//total_length
+        
+        return res
