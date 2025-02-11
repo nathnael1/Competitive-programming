e7d8f169e965c945d826f1dabb5a4cede1cb02d6
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-11 05:07:16 +0300

    Time: 3 ms (13.29%), Space: 17.9 MB (11.31%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,9 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(0,len(nums)-1):
-            for j in range(0, len(nums)-1 - i):
+        #using insertion sort
+        for i in range(1,len(nums)):
+            for j in range(i-1,-1,-1):
                 if nums[j] > nums[j+1]:
-                    nums[j], nums[j+1] = nums[j+1], nums[j]
-                
-        
+                    nums[j],nums[j+1] = nums[j+1],nums[j]
+                else:
+                    break
+
+        #using buble sort
+        # for i in range(len(nums)):
+        #     for j in range(len(nums)-1-i):
+        #         if nums[j] > nums[j+1]:
+        #             nums[j],nums[j+1] = nums[j+1],nums[j]
+
+        # #using selection sort
+        # for i in range(len(nums)):
+        #     min_index = i
+        #     for j in range(i+1,len(nums)):
+        #         if nums[j] < nums[min_index]:
+        #             min_index = j
+        #     if min_index != i:
+        #         nums[i],nums[min_index] = nums[min_index], nums[i]
