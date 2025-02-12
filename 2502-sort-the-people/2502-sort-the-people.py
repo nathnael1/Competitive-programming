b52983271cf207472869ecec07f8993b147c1284
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-12 09:20:38 +0300

    Time: 1 ms (88.72%), Space: 18.1 MB (66.37%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -1,9 +1,7 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        #sorting using bubble sort
-        for i in range(0,len(names)-1):
-            for j in range(0,len(names)-i-1):
-                if heights[j] < heights[j+1]:
-                    names[j],names[j+1] = names[j+1],names[j]
-                    heights[j],heights[j+1] = heights[j+1], heights[j]
-        return names
+        #sorting using insertion sort
+        
+        zipped = list(zip(names,heights))
+        zipped.sort(key = lambda x: x[1],reverse = True)
+        return [name for name,height in zipped]
