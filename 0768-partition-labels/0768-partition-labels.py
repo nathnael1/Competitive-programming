1fe51c0097daeb7fbb34e959f9d6d9b34dbdb782
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-12 16:37:17 +0300

    Time: 20 ms (5.07%), Space: 18 MB (13.84%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.py
@@ -0,0 +1,26 @@
+from collections import Counter
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        #counting the frequency
+        counter = Counter(s)
+
+        res = []
+        pointer = 0
+        temp_value = []
+        #we must have  0 value for each dictionary
+        for i in range(len(s)):
+            if counter[s[i]] == 1:
+                counter.pop(s[i])
+            else:
+                counter[s[i]]-=1
+            temp_value.append(s[i])
+            found = False
+            for c in "".join(temp_value):
+                if counter[c]!=0:
+                    found = True
+                    break
+            if not found:
+                res.append(len(temp_value))
+                temp_value = []
+        return res
+                
