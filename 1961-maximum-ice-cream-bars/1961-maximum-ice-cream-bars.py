62da07db4b5cf8fb00f928665450b8d702fef0e5
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-12 14:30:09 +0300

    Time: 82 ms (46.52%), Space: 29 MB (78.94%) - LeetHub

--- 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.py
+++ 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.py
@@ -1,21 +1,13 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        modified_array = [0] * (max(costs) + 1)
-        output_array = [0] * len(costs)
-        for i in range(len(costs)):
-            modified_array[costs[i]] +=1
-        for i in range(1,len(modified_array)):
-            modified_array[i] += modified_array[i-1]
-        for i in costs[::-1]:
-            value = modified_array[i] -1
-            modified_array[i]-=1
-            output_array[value] = i 
-        icecream = 0
-        total = 0
-        for i in output_array:
-            total += i
-            if total <= coins:
-                icecream+=1
-            else:
+        #greddy approach
+        costs.sort()
+        res  =0
+        value = 0
+        #checking if the coin is greater than the cost
+        for price in costs:
+            value+=price
+            if value > coins:
                 break
-        return icecream
+            res+=1
+        return res
