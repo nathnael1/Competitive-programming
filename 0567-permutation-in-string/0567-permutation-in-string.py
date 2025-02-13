34f5b8d5db73ea9b7fec4dd952ee90d3be6a6586
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-13 16:45:13 +0300

    Time: 147 ms (22.16%), Space: 18.1 MB (18.18%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,10 +1,26 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         #checking counter for every slice
+        if len(s2) < len(s1):
+            return False
         n = len(s1)
-        p = 0
-        while p < len(s2)-n+1:
-            if Counter(s2[p:p+n]) == Counter(s1):
+        counter_s1 = Counter(s1)
+        counter_s2 = defaultdict(int)
+        for i in range(n):
+            counter_s2[s2[i]]+=1
+        #itterating through s2
+        l = 0
+
+        for r in range(n,len(s2)):
+            print(counter_s2)
+            if counter_s1 == counter_s2:
                 return True
-            p+=1
+            
+            counter_s2[s2[r]]+=1
+            counter_s2[s2[l]]-=1
+            if counter_s2[s2[l]] == 0:
+                counter_s2.pop(s2[l])
+            l+=1
+        if counter_s1 == counter_s2:
+                return True
         return False
