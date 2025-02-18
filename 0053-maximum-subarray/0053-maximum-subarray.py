4a5515574fc359f00be156236391d0953abac555
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-18 11:23:49 +0300

    Time: 208 ms (5.04%), Space: 33.2 MB (7.6%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,9 +1,14 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         #using kaden's algorithm
-        max_value_list = nums[0]
-        max_value = nums[0]
-        for i in range(1,len(nums)):
-            max_value_list = max(max_value_list+nums[i],nums[i])
-            max_value = max(max_value,max_value_list)
-        return max_value
+        res = float('-inf')
+        sub_array = 0
+        for i in range(len(nums)):
+            sub_array+= nums[i]
+            if sub_array < nums[i]:
+                res = max(res,nums[i])
+                sub_array = nums[i]
+            else:
+                res = max(res,sub_array)
+            print(res)
+        return res
