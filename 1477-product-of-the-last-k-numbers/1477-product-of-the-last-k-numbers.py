4528e740b58394f7ad626fef74f5baa82950ee2a
Nathnael Tamirat <81173345+nathnaeltamirat@users.noreply.github.com>
2025-02-21 11:02:39 +0300

    Time: 57 ms (18.13%), Space: 31.9 MB (23.12%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.py
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.py
@@ -2,13 +2,38 @@ class ProductOfNumbers:
 
     def __init__(self):
         self.nums = []
-
+        self.product = []
+        self.pointer = -1
+        self.zero = None
     def add(self, num: int) -> None:
         self.nums.append(num)
+        if self.product and self.product[-1] != 0:
+            self.product.append(num* self.product[-1])
+            self.pointer+=1
+        elif not self.product:
+            self.product.append(num)
+            self.pointer+=1
+        elif self.product and self.product[-1] == 0:
+            self.product.append(num)
+            self.pointer+=1
+        if num == 0:
+            self.zero = self.pointer
+            
 
     def getProduct(self, k: int) -> int:
-        product = prod(self.nums[-k:])
-        return product
+        if self.zero is not None:
+            recent_zero = self.zero - len(self.product)
+            if -k - 1 == recent_zero:
+                return self.product[-1]
+            elif -k-1 > recent_zero:
+                return self.product[-1]//self.product[-k-1]
+            else:
+                return 0
+        else:
+            if k < len(self.product):
+                return self.product[-1]//self.product[-k-1]
+            if k == len(self.product):
+                return self.product[-1]
 
 
 
